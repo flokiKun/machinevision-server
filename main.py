b2838a85d896fab8468fe12ce6877be39fd1383b
Viktor <viktorttyt@gmail.com>
2018-10-04 11:54:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import opencv
-import tornado
---Make code 
+import cv2
+import tornado.ioloop
+from tornado.iostream import IOStream
+import numpy as np
+
+#Main FUnctions
+ 
+
+def main():
+    #obj_cascade = cv2.CascadeClassifier('obj_cascade_default.xml')
+
+if prep_ok:
+        main()
+        tornado.ioloop.IOLoop.instance().start()
