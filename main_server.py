d6c0984d6c458ed6b2722d847b161d203a6dc483
Viktor <viktorttyt@gmail.com>
2018-10-11 13:01:48 +0000

    new thread server

--- main_server.py
+++ main_server.py
@@ -1,20 +1,38 @@
 import socket
+from _thread import *
 import sys
-import tornado.ioloop
 
-s = socket.socket()
+
+tpc_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = 'localhost'
-port = 8007
-s.bind((host, port))
-s.listen(1)
-conn, addr = s.accept()
+port = 80
+
+try:
+    tpc_socket.bind((host, port))
+except socket.error as e:
+    print(str(e))
+
+def threaded_client(conn):
+    conn.send(str.encode('[SEVER]Welcome, send your data\n'))
+    while True:
+        data = conn.recv(2048)
+        reply = '[SERVER]Server output: ' + data.decode('utf-8')
+        if not data:
+            break
+        conn.sendall(str.encode(reply))
+    conn.close()
+
+tpc_socket.listen(5)
+
+print('[SERVER]Waiting for a connection...')
 
 def main():
-
-    data = conn.recv(50)
-    print('client is at',addr,data)
+    conn, addr = tpc_socket.accept()
+    data = conn.recv(2048)
+    print('[SERVER]New data from {}:{}'.format(addr[0],addr[1]))
+    print('[SERVER]Recv:{}'.format(data.decode('utf-8')))
+    start_new_thread(threaded_client, (conn,))
     #conn.send(data)
 
-main()
-tornado.ioloop.IOLoop.current().start()
-conn.close()
+while True:
+    main()
