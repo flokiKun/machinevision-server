cdc079fcd3ba5145a0b46355a5644fed95066c7d
Viktor <viktorttyt@gmail.com>
2018-10-11 13:25:22 +0000

    just test client

--- py_client.py
+++ py_client.py
@@ -2,15 +2,10 @@ import socket
 import sys
 import tornado.ioloop
 
-s = socket.socket()
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = 'localhost'
-port = 8007
-
-def main():
-    s.connect((host, port))
-    pack=bytes(str(input('> ')), 'utf-8')
-    s.send(pack)
-    #data = s.recv(1000000)
-
-main()
-tornado.ioloop.IOLoop.current().start()
+port = 80
+s.connect((host, port))
+pack = bytes(str(input('> ')), 'utf-8')
+s.send(pack)
+#data = s.recv(1000000)
