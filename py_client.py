20e335b052b7e98a48c6741200be0ff62a8bb5f9
Viktor <viktorttyt@gmail.com>
2018-10-22 11:55:49 +0000

    jojo client

--- py_client.py
+++ py_client.py
@@ -1,11 +1,11 @@
 import socket
 import sys
-import tornado.ioloop
+#import tornado.ioloop
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = 'localhost'
-port = 80
+port = 1337
 s.connect((host, port))
-pack = bytes(str(input('> ')), 'utf-8')
+pack = bytes(' { "request_code":"reqimg", "query":"ya eblan", "rtcount":"5" }', 'utf-8')
 s.send(pack)
 #data = s.recv(1000000)
